5e6f8eb315591258e1ee8d8cc842301cdaa5ab46
hen <hengolubenko@gmail.com>
2024-12-25 09:31:45 +0200

    final

--- client2/logger.py
+++
@@ -1,7 +0,0 @@
-import logging
-import os
-
-log_level = logging.DEBUG if os.getenv('DEBUG') else logging.INFO
-
-logging.basicConfig(level=log_level, format='%(levelname)s - %(message)s')
-logger = logging.getLogger()
